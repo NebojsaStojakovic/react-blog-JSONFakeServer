7152d90739138a10af6690e73f6e18dffe668015
NebojsaStojakovic <nebojsastojakovicla@gmail.com>
2022-06-13 10:35:39 +0200

    user details

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,25 +1,34 @@
 import React, { useEffect, useState } from "react";
 import { getFirestore } from "firebase/firestore";
 import { firebaseApp } from "../firebase-config";
-import { getAllFeeds } from "../utils/fetchData";
+import { categoryFeeds, getAllFeeds } from "../utils/fetchData";
 import Spinner from "./Spinner";
 import { SimpleGrid } from "@chakra-ui/react";
 import VideoPin from "./VideoPin";
 import NotFound from "./NotFound";
+import { useParams } from "react-router-dom";
 
 const Feed = () => {
+  const firestoreDb = getFirestore(firebaseApp);
+
   const [feeds, setFeeds] = useState(null);
   const [loading, setLoading] = useState(false);
-
-  const firestoreDb = getFirestore(firebaseApp);
+  const { categoryId } = useParams();
 
   useEffect(() => {
     setLoading(true);
-    getAllFeeds(firestoreDb).then((data) => {
-      setFeeds(data);
-      setLoading(false);
-    });
-  }, []);
+    if (categoryId) {
+      categoryFeeds(firestoreDb, categoryId).then((data) => {
+        setFeeds(data);
+        setLoading(false);
+      });
+    } else {
+      getAllFeeds(firestoreDb).then((data) => {
+        setFeeds(data);
+        setLoading(false);
+      });
+    }
+  }, [categoryId]);
 
   if (loading) return <Spinner />;
   if (!feeds?.length > 0) return <NotFound />;
